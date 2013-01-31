89210318ddc277c7163f69a72b831d9c260ea08b
patrick <pmary@patrick-star.(none)>
2013-01-30 21:04:58 -0800

    Merge branch 'master' of https://github.com/tonymx227/echek.fr
    sons

--- js/script.js
+++ js/script.js
@@ -45,6 +45,50 @@ for (var i=5;i<15;i++){
 //map.addPersonnage(p3);
 //map.addPersonnage(p);
 
+
+var preload;
+        
+function init(){
+    
+    createjs.FlashPlugin.BASE_PATH = 'js/soundjs/src/soundjs/';
+    
+    if (!createjs.SoundJS.checkPlugin(true)) {
+        alert('Imposible de chargé les plugins');
+        return;
+    }
+
+    var manifest = [
+        {src:'sounds/main.mp3', id:1, data: 1}
+    ];
+
+    preload = new createjs.PreloadJS();
+    preload.installPlugin(createjs.SoundJS);
+    
+    preload.onFileLoad = function(event) {
+        console.log('Le son est chargé, prêt à lire !');
+    };
+
+    preload.onComplete = function(event) {
+        console.log('Le son est complété !');
+    }
+
+    preload.loadManifest(manifest, true);
+}
+
+function stopSound() {
+    if (preload != null) { preload.close(); }
+    createjs.SoundJS.stop();
+}
+
+function playSound(id) {
+    var instance = createjs.SoundJS.play(id, createjs.SoundJS.INTERRUPT_NONE, 0, 0, false, 1);
+    if (instance == null || instance.playState == createjs.SoundJS.PLAY_FAILED) { return; }
+    instance.onComplete = function(instance) {
+        console.log('il est finis de lire');
+    }
+
+}
+
 window.addEventListener('load', function(){
 
     init();
